4255fdf16822a4c90bed5c697100b7a642ec6aa1
lih8697-1 <lih8697@gmail.com>
2026-04-01 09:36:13 +0900

    57-payload 함수 구현

--- src/test/java/com/back/domain/member/service/AuthTokenServiceTest.java
+++ src/test/java/com/back/domain/member/service/AuthTokenServiceTest.java
@@ -3,9 +3,6 @@ package com.back.domain.member.service;
 import com.back.domain.member.entity.Member;
 import com.back.domain.member.repository.MemberRepository;
 import com.back.standard.ut.Ut;
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.security.Keys;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,9 +10,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.crypto.SecretKey;
-import java.nio.charset.StandardCharsets;
-import java.util.Date;
 import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -41,39 +35,18 @@ public class AuthTokenServiceTest {
 
     @Test
     @DisplayName("jjwt 최신 방식으로 JWT 생성, {name=\"Paul\", age=23}")
-    void t2() {
-        // 토큰 만료기간: 1년
-        long expireMillis = 1000L * 60 * 60 * 24 * 365;
-
-        byte[] keyBytes = "abcdefghijklmnopqrstuvwxyz1234567890abcdefghijklmnopqrstuvwxyz1234567890"
-                .getBytes(StandardCharsets.UTF_8);
-        SecretKey secretKey = Keys.hmacShaKeyFor(keyBytes);
-
-        // 발행 시간과 만료 시간 설정
-        Date issuedAt = new Date();
-        Date expiration = new Date(issuedAt.getTime() + expireMillis);
+    void t2() throws InterruptedException {
 
         Map<String, Object> payload = Map.of("name", "Paul", "age", 23);
 
-        String jwt = Jwts.builder()
-                .claims(payload) // 내용
-                .issuedAt(issuedAt) // 생성날짜
-                .expiration(expiration) // 만료날짜
-                .signWith(secretKey) // 키 서명
-                .compact();
+        String jwt = Ut.jwt.toString(secretPattern, expireSeconds, payload);
+        Map<String, Object> parsedPayload = Ut.jwt.payload(jwt, secretPattern);
 
-        // jwt 확인(파싱)
-        Claims parsedPayload = Jwts
-                .parser()
-                .verifyWith(secretKey)
-                .build()
-                .parseSignedClaims(jwt)
-                .getPayload();
-
-        assertThat(parsedPayload.get("name", String.class)).isEqualTo("Paul");
-        assertThat(parsedPayload.get("age", Integer.class)).isEqualTo(23);
+        assertThat(parsedPayload)
+                .containsAllEntriesOf(payload);
 
         assertThat(jwt).isNotBlank();
+
 
         System.out.println("jwt = " + jwt);
     }
