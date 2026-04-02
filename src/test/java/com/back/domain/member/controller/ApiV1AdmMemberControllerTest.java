4f1106a52081c72da6ad8d28622c8d514def6430
lih8697-1 <lih8697@gmail.com>
2026-04-02 12:06:06 +0900

    77-회원 다건 조회 관리자 권한 여부에 따른 접근 제한

--- src/test/java/com/back/domain/member/controller/ApiV1AdmMemberControllerTest.java
+++ src/test/java/com/back/domain/member/controller/ApiV1AdmMemberControllerTest.java
@@ -1,5 +1,6 @@
 package com.back.domain.member.controller;
 
+import com.back.domain.member.entity.Member;
 import com.back.domain.member.repository.MemberRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -11,12 +12,10 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.transaction.annotation.Transactional;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.hamcrest.Matchers.containsInRelativeOrder;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.handler;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 
 @SpringBootTest
@@ -69,5 +68,28 @@ public class ApiV1AdmMemberControllerTest {
 //                    .andExpect(jsonPath("$[%d].title".formatted(i)).value(post.getTitle()))
 //                    .andExpect(jsonPath("$[%d].content".formatted(i)).value(post.getContent()));
 //        }
+    }
+
+    @Test
+    @DisplayName("회원 다건 조회, 권한이 없는 경우")
+    void t2() throws Exception {
+
+        Member actor = memberRepository.findByUsername("user1").get();
+
+        ResultActions resultActions = mvc
+                .perform(
+                        get("/api/v1/adm/members")
+                                .header(
+                                        "Authorization", "Bearer %s".formatted(actor.getApiKey())
+                                )
+                )
+                .andDo(print());
+
+        resultActions
+                .andExpect(handler().handlerType(ApiV1AdmMemberController.class))
+                .andExpect(handler().methodName("list"))
+                .andExpect(status().isForbidden())
+                .andExpect(jsonPath("$.resultCode").value("403-1"))
+                .andExpect(jsonPath("$.msg").value("권한이 없습니다."));
     }
 }
