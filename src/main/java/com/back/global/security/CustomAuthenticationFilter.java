5681f02bc5778b1f335f6d1fcec3a7a04e5a6a97
lih8697-1 <lih8697@gmail.com>
2026-04-03 14:37:57 +0900

    92-관리자 API 권한 검사를 Spring Security 역할 기반으로 전환

--- src/main/java/com/back/global/security/CustomAuthenticationFilter.java
+++ src/main/java/com/back/global/security/CustomAuthenticationFilter.java
@@ -126,12 +126,12 @@ public class CustomAuthenticationFilter extends OncePerRequestFilter {
                 member.getUsername(),
                 member.getPassword(),
                 member.getNickname(),
-                List.of()
+                member.getAuthorities()
         );
 
         Authentication authentication = new UsernamePasswordAuthenticationToken(
                 user,
-                null,
+                user.getPassword(),
                 user.getAuthorities()
         );
 
